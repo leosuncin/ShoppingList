6b6ae3d72226785e28cf1cb413abe7ae9982cdc1
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-02-04 10:53:37 -0600

    Render list items

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {FlatList, StyleSheet, View} from 'react-native';
+import {uuid} from 'uuidv4';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {
+      id: uuid(),
+      text: 'Milk',
+    },
+    {
+      id: uuid(),
+      text: 'Eggs',
+    },
+    {
+      id: uuid(),
+      text: 'Bread',
+    },
+    {
+      id: uuid(),
+      text: 'Juice',
+    },
+  ]);
+
   return (
     <View style={styles.container}>
       <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem
+            item={item}
+            onDelete={() =>
+              setItems(prevItems =>
+                prevItems.filter(element => element.id !== item.id),
+              )
+            }
+          />
+        )}
+      />
     </View>
   );
 };
