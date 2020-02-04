a9951c5632410e1522435c0338702bb85ea1b7da
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-02-04 10:18:22 -0600

    Add custom header

--- App.js
+++ App.js
@@ -1,31 +1,17 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-      <Image
-        source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}
-        style={styles.img}
-      />
+      <Header />
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    color: 'darkslateblue',
-    fontSize: 30,
-  },
-  img: {
-    width: 100,
-    height: 100,
-    borderRadius: 100 / 2,
   },
 });
 
